715256e09a1e5b0872266df34db5a362af0b969b
Dihan <dihan.algama@gmail.com>
2025-03-03 22:20:00 +0000

    change port

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ def download_video(filepath):
     return send_from_directory(directory, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8181, debug=True)
+    app.run(host='0.0.0.0', port=8282, debug=True)
